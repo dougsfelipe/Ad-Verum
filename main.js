65f65374546c0ab9c54e11bc3d11217e370c094e
Douglas Felipe <38046081+dougsfelipe@users.noreply.github.com>
2021-07-25 20:32:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ function IrCursos() {
 function somar(){
     var s1 = parseInt(document.getElementById('teste2').innerHTML, 10);
     var s1 = s1 + 1;
+    localStorage.setItem('verum:new-prouct', 
+    JSON.stringify(s1))
     document.getElementById("teste2").innerHTML = s1;
     
     }
